c4ec6796b2e7e91e021c051160903827f8713fa2
mejerr <georgy_90@abv.bg>
2022-08-04 11:21:23 +0300

    Initialize project and metamask connection + smart contracts

---
+++ src/App.svelte
@@ -0,0 +1,167 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { ethers, logger } from "ethers";
+
+  let connectWalletError: any;
+  const { ethereum } = window;
+
+	let provider: any = ethereum;
+
+	let userAddress: string = '';
+	let userBalance: number = 0;
+	let signer: any = null;
+	let library: any = null;
+	let connected: boolean = false;
+	let chainId: number = 1;
+	let network: string = '';
+	let contractsSDK: any = null;
+
+
+	const changedAccount = async (accounts: string[]) => {
+    if(!accounts.length) {
+      // Metamask Lock fire an empty accounts array
+      await resetApp();
+    } else {
+			connected = true;
+			userAddress = accounts[0];
+      window.location.reload();
+    }
+  }
+
+	const networkChanged = async () => {
+		if (provider) {
+			library = new ethers.providers.Web3Provider(ethereum);
+      const connectedNetwork = await library.getNetwork();
+      chainId = connectedNetwork.chainId;
+			network = connectedNetwork.name;
+    }
+  }
+
+	const close = async () => {
+    resetApp();
+  }
+
+	const unSubscribe = async (provider: any) => {
+    // Workaround for metamask widget > 9.0.3 (provider.off is undefined);
+    window.location.reload();
+    if (!provider.off) {
+      return;
+    }
+
+    provider.off("accountsChanged", changedAccount);
+    provider.off("networkChanged", networkChanged);
+    provider.off("close", close);
+  }
+
+  const resetApp = async (/*{ onSuccess = () => {} } = {}*/) => {
+  	localStorage.removeItem("WEB3_CONNECT_CACHED_PROVIDER");
+  	localStorage.removeItem("cachedProvider");
+
+  	// onSuccess();
+
+  	await unSubscribe(ethereum);
+
+		provider = null;
+		userAddress = '';
+		userBalance = 0;
+		signer = null;
+		library = null;
+		connected = false;
+		chainId = 1;
+		network = '';
+  };
+
+	const subscribeToProviderEvents = async (provider: any) => {
+		if (!provider.on) {
+			return;
+    }
+
+    provider.on("accountsChanged", changedAccount);
+    provider.on("chainChanged", networkChanged);
+    provider.on("disconnect", close);
+  };
+
+	const connectWallet = async () => {
+		if (!localStorage.getItem('WEB3_CONNECT_CACHED_PROVIDER')) {
+			localStorage.setItem('WEB3_CONNECT_CACHED_PROVIDER', 'injected');
+			localStorage.setItem('cachedProvider', 'true');
+		}
+    connected = false;
+
+    await ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then(async (accountList: any) => {
+        const [firstAccount] = accountList;
+        userAddress = firstAccount;
+        connected = true;
+
+        const connectedLibrary: any = new ethers.providers.Web3Provider(ethereum);
+				const connectedSigner = connectedLibrary.getSigner();
+				console.log( await connectedLibrary.getNetwork());
+				network = (await connectedLibrary.getNetwork()).name;
+        userBalance = +ethers.utils.formatEther((await connectedSigner.getBalance()).toString());
+        chainId = await connectedSigner.getChainId();
+	  		library = connectedLibrary;
+	  		signer = connectedSigner;
+	  		connected = true;
+
+				await subscribeToProviderEvents(provider);
+      })
+      .catch((error: any) => {
+        connected = false;
+        connectWalletError = error;
+        console.log("error connecting wallet");
+      });
+  };
+
+  onMount(() => {
+    const initConnection = async () => {
+      await connectWallet();
+    };
+
+    if (localStorage.getItem('cachedProvider')) {
+      initConnection();
+    }
+  });
+</script>
+
+<main>
+  {#if connected}
+    <div>
+      <span class="dotConnected" />
+      Connected Account: {userAddress}
+      Connected userBalance: {userBalance}
+      Connected chainId: {chainId}
+      Connected network: {network}
+      <button on:click={resetApp}>Disconnect:</button>
+    </div>
+  {:else}
+    <button class="button buttonMetaMask" on:click={connectWallet}>
+      Connect MetaMask
+    </button>
+  {/if}
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  button {
+    width: 150px;
+    height: 150px;
+    border-radius: 10px;
+    border: 1px solid black;
+    background: #ff3e00;
+    cursor: pointer;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import App from './App.svelte';
+
+declare global {
+	// tslint:disable-next-line
+	interface Window {
+	  web3: any;
+	  ethereum: any;
+	  Web3Modal: any;
+	  Box: any;
+	  box: any;
+	  space: any;
+	}
+}
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+export default app;
